001847a83eb0024b70509e77cf553b4abeb38b45
Bisma Muqeed <bismamuqeed@gmail.com>
2024-06-24 10:37:41 -0700

    assignments

---
+++ Question3/demo3.js
@@ -0,0 +1,13 @@
+"use strict";
+// Write a function fetchData that returns a Promise which resolves with the string
+// "Data fetched successfully!" after a delay of 1 second.
+let fetchData = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Data fetched succcesfully");
+        }, 1000);
+    });
+};
+fetchData().then((resolve) => {
+    console.log(resolve);
+});
